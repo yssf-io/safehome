4273461873c5405ebe7424a2c833982f401a4ac8
yssf-io <hey@yssf.io>
2023-09-22 21:15:16 -0400

    added connect wallet with Web3Modal

--- webapp/src/app/page.tsx
+++ webapp/src/app/page.tsx
@@ -1,9 +1,38 @@
-import Image from "next/image";
+"use client";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Button, Web3Modal } from "@web3modal/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+
+const chains = [arbitrum, mainnet, polygon];
+const projectId = "1b02254489e1baf6c7e25adbacd8a731";
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient,
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>hello world</h1>
-    </main>
+    <div>
+      <WagmiConfig config={wagmiConfig}>
+        <main className="">
+          <h1>hello world</h1>
+
+          <div className="absolute top-5 right-5">
+            <Web3Button />
+          </div>
+        </main>
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </div>
   );
 }
